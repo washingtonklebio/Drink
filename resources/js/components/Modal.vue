c7e5afe4fb384f08efc63523ea05e72ea12e4942
Washington <washington@localhost.localdomain>
2019-10-18 13:05:50 -0300

    Criação de componente modal e tabela de refrigerantes

---
+++ resources/js/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-dialog v-model="show" persistent max-width="35rem">
+    <v-card>
+      <v-toolbar dark color="primary">
+        <v-toolbar-title><v-icon>{{ headerIcon }}</v-icon> {{ header }}</v-toolbar-title>
+        <span class="close" @click.stop="show=false">x</span>
+      </v-toolbar>
+      <v-card-text>
+        <v-container>
+          <slot></slot>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" @click.stop="show=false">Salvar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Boolean,
+    header: String,
+    headerIcon: String
+  },
+  computed: {
+    show: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+  .close {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    font-size: 1.2rem;
+    font-weight: 600;
+    height: 20px;
+    width: 20px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+</style>
--- resources/js/components/refrigerante/Refrigerante.vue
+++ resources/js/components/refrigerante/Refrigerante.vue
@@ -1,82 +1,135 @@
 <template>
-    <v-container>
-        <v-card>
-            <v-card-title>
-            Refrigerantes
-            <v-spacer></v-spacer>
-            <v-text-field
-                v-model="search"
-                append-icon="search"
-                label="Pesquisar    "
-                single-line
-                hide-details
-            ></v-text-field>
-            </v-card-title>
-            <v-data-table
-            :headers="headers"
-            :items="desserts"
-            :search="search"
-            ></v-data-table>
-        </v-card>
-        <v-btn bottom color="primary" dark fab fixed right
-        @click=""
-        >
-        <v-icon>mdi-plus</v-icon>
-        </v-btn>
-    </v-container>
+  <v-container>
+    <v-alert v-if="!desserts"
+      border="left"
+      colored-border
+      type="info"
+      elevation="2"
+    >Nenhum refrigerante cadastrado.</v-alert>
+    <v-card v-else>
+      <v-card-title>
+        Refrigerantes
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="search"
+          label="Pesquisar"
+          single-line
+          hide-details>
+          </v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :search="search"
+        :items="desserts"
+        :page.sync="page"
+        :items-per-page="itemsPerPage"
+        hide-default-footer
+        class="elevation-1"
+        @page-count="pageCount = $event"
+      ></v-data-table>
+      <div class="text-center pt-2">
+        <v-pagination v-model="page" :length="pageCount"></v-pagination>
+      </div>
+    </v-card>
+    <v-btn bottom color="primary" dark fab fixed right @click.stop="showModal=true">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <Modal v-model="showModal" :header="titleModal" :headerIcon="iconModal">
+      <v-row>
+          <v-col cols="12" sm="6" md="6">
+            <v-text-field label="Marca" required></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <v-select
+              :items="liters"
+              label="Litragem"
+              required
+            ></v-select>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <v-select
+              :items="types"
+              label="Tipo"
+              required
+            ></v-select>
+          </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field label="Quantidade" required></v-text-field>
+        </v-col>
+      </v-row>
+    </Modal>
+  </v-container>
 </template>
 <script>
-export default {
+  import Modal from "../Modal"
+
+  export default {
+    name: "Refrigerante",
+    components: { Modal },
     data () {
       return {
-        search: '',
+        page: 1,
+        pageCount: 0,
+        itemsPerPage: 1,
+        search: "",
+        titleModal: 'Adicionar',
+        iconModal: 'fa fa-pencil-square-o',
+        showModal: false,
+        liters: ['250ml', '600ml', '1L', '1.5L', '2L', '3L'],
+        types: ['Pet', 'Garrafa', 'Lata'],
+        search: "",
         headers: [
           {
-            text: 'Marca',
-            align: 'left',
+            text: "Marca",
+            align: "left",
             sortable: false,
-            value: 'marca',
+            value: "marca"
           },
-          { text: 'Tipo', value: 'tipo' },
-          { text: 'Sabor', value: 'sabor' },
-          { text: 'Litragem', value: 'litragem' },
-          { text: 'Valor da unidade (R$)', value: 'valor' },
+          { text: "Tipo", value: "tipo" },
+          { text: "Sabor", value: "sabor" },
+          { text: "Litragem", value: "litragem" },
+          { text: "Valor da unidade (R$)", value: "valor" },
+          {
+            text: "Ações",
+            sortable: false
+          }
         ],
         desserts: [
           {
-            marca: 'São gerardo',
-            tipo: 'Guaraná',
-            sabor: 'Cajú',
-            litragem: '1,5 ml',
-            valor: 5,
+            marca: "São gerardo",
+            tipo: "Guaraná",
+            sabor: "Cajú",
+            litragem: "1,5 ml",
+            valor: 5
           },
           {
-            marca: 'São gerardo',
-            tipo: 'Guaraná',
-            sabor: 'Cajú',
-            litragem: '1,5 ml',
-            valor: 5,
+            marca: "Coca Cola",
+            tipo: "Guaraná",
+            sabor: "Cajú",
+            litragem: "1,5 ml",
+            valor: 5
           },
           {
-            marca: 'São gerardo',
-            tipo: 'Guaraná',
-            sabor: 'Cajú',
-            litragem: '1,5 ml',
-            valor: 5,
+            marca: "São gerardo",
+            tipo: "Guaraná",
+            sabor: "Cajú",
+            litragem: "1,5 ml",
+            valor: 5
           },
           {
-            marca: 'São gerardo',
-            tipo: 'Guaraná',
-            sabor: 'Cajú',
-            litragem: '1,5 ml',
-            valor: 5,
-          },
-        ],
+            marca: "São gerardo",
+            tipo: "Guaraná",
+            sabor: "Cajú",
+            litragem: "1,5 ml",
+            valor: 5
+          }
+        ]
       }
     },
   }
 </script>
-
 <style scoped>
-
 </style>
