684e2b178eef3c5f275e778cc7b65efa145bf01a
Washington <washington@localhost.localdomain>
2019-10-25 13:49:17 -0300

    Finalização de table refrigerantes

--- resources/js/components/home/Home.vue
+++ resources/js/components/home/Home.vue
@@ -8,14 +8,7 @@
 export default {
     data(){
         return {
-            title:true,
-            columns: [
-                {text:"Nome", sortable: true, value: "name", align:"center"},
-                {text:"Email", sortable: true, value: "email", align:"center"},
-                {text:"Whatsapp", sortable: true, value: "phone", align:"center"},
-                {text:"CNPJ",sortable:true,value:"cnpj",align:"center"},
-                {text:"CPF",sortable:true,value:"cpf",align:"center"},
-            ],
+            
         }
     },
 }
